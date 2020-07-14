e1301a7413df3c429deb3011386000aac5f3d781
Sean P. Myrick V19.0.3.1 <65933340+seanpm2001@users.noreply.github.com>
2020-07-14 16:10:38 -0700

    Germany_levelsV1.py
    An archive of the first version of the list of Germany themed levels.

---
+++ Game/iBlast3/Game/Around_the_world_campaign_mode/Europe/Germany/OldVersions/Germany_levelsV1.py
@@ -0,0 +1,48 @@
+# Start of script
+
+"""
+Around the world campaign / Europe / Germany
+This is a set of levels for the around the world campaign that centers around Germany themed levels.
+"""
+
+confirmExec1 = input("This file is not meant to be ran. Please run the main game file to play the game, or press [ENTER] to learn more")
+print("Level list")
+# Please help me come up with better names
+"""
+Note:
+References to Nazi germany and World War I Germany will only be accessible as add-ons, they cannot be in the actual game by default.
+Add-ons may have to get modified to be allowed into Germany, swastikas will have to be replaced with something else. The add-on also cannot IN ANY WAY glorify, defend, or promote Adolf Hitler, other Nazi figures, and Nazism in general. It also cannot defend Germanys actions in World War I. That is NOT what this game is about.
+I am leaving this in as a historical idea, so that you can rescue Jewish Mokis, and do other missions.
+"""
+print("01 - Untitled")
+print("02 - Untitled")
+print("03 - Untitled")
+print("04 - Untitled")
+print("05 - Untitled")
+print("06 - Untitled")
+print("07 - Untitled")
+print("08 - Untitled")
+print("09 - Untitled")
+print("10 - Untitled")
+print("11 - Untitled")
+print("12 - Untitled")
+print("13 - Untitled")
+print("14 - Untitled")
+print("15 - Untitled")
+print("16 - Untitled")
+print("17 - Untitled")
+print("18 - Untitled")
+print("19 - Untitled")
+print("20 - Untitled")
+# I might add more levels
+noMoreExit = input("Press [ENTER] to close this file")
+print("This text should not stay on your screen. If it is, wait a bit, or close the window. If that doesn't work, kill the task with your task manager, system monitor, or terminal")
+
+'''
+File info
+
+File version: 1 (Tuesday, July 14th 2020 at 4:10 pm)
+File type: Python 3.7 (*.py)
+Line count (including blank lines and compiler line): 49
+'''
+# End of script

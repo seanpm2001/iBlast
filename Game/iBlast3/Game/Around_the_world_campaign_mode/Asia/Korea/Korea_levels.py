122fcb18449adcb83414263852c33482137dbbf4
Sean P. Myrick V19.0.3.1 <65933340+seanpm2001@users.noreply.github.com>
2020-07-14 16:05:02 -0700

    Korea_levels.py
    The current version (version 1) of the list of Korean levels, in Python format.

---
+++ Game/iBlast3/Game/Around_the_world_campaign_mode/Asia/Korea/Korea_levels.py
@@ -0,0 +1,48 @@
+# Start of script
+
+"""
+Around the world campaign / asia / Korea
+This is a set of levels for the around the world campaign that centers around Korean themed levels.
+"""
+
+confirmExec1 = input("This file is not meant to be ran. Please run the main game file to play the game, or press [ENTER] to learn more")
+print("Level list")
+# Please help me come up with better names
+"""
+Note:
+I am currently deciding if North Korea can be mentioned or not, I think it should be a set of hidden levels that have some Easter Egg that activates them.
+When this is done, it has to fit the puzzle theme, and cannot have North Korean citizens dying to the Regime, it has to be more friendly, even though being friendly is definitely not North Koreas thing.
+With that, I am still questioning if it should even be added. Maybe a mission to disarm Nuclear missiles? I don't want this game to focus on Korean politics.
+"""
+print("01 - Untitled")
+print("02 - Untitled")
+print("03 - Untitled")
+print("04 - Untitled")
+print("05 - Untitled")
+print("06 - Untitled")
+print("07 - Untitled")
+print("08 - Untitled")
+print("09 - Untitled")
+print("10 - Untitled")
+print("11 - Untitled")
+print("12 - Untitled")
+print("13 - Untitled")
+print("14 - Untitled")
+print("15 - Untitled")
+print("16 - Untitled")
+print("17 - Untitled")
+print("18 - Untitled")
+print("19 - Untitled")
+print("20 - Untitled")
+# I might add more levels
+noMoreExit = input("Press [ENTER] to close this file")
+print("This text should not stay on your screen. If it is, wait a bit, or close the window. If that doesn't work, kill the task with your task manager, system monitor, or terminal")
+
+'''
+File info
+
+File version: 1 (Tuesday, July 14th 2020 at 4:05 pm)
+File type: Python 3.7 (*.py)
+Line count (including blank lines and compiler line): 49
+'''
+# End of script

d8061c1d60500998d062f7e1450cda35b4f0619e
Sean P. Myrick V19.0.3.1 <65933340+seanpm2001@users.noreply.github.com>
2020-07-14 16:00:44 -0700

    Japan_levelsV1.py
    An archive of the first version of the list of Japan themed levels. I submitted the last commit too early, I was typing, and I accidentally pressed the button combination that commits the file. This keeps happening, then I have to explain it in my next commit.

---
+++ Game/iBlast3/Game/Around_the_world_campaign_mode/Asia/Japan/OldVersions/Japan_levelsV1.py
@@ -0,0 +1,42 @@
+# Start of script
+
+"""
+Around the world campaign / asia / Japan
+This is a set of levels for the around the world campaign that centers around Japanese themed levels.
+"""
+
+confirmExec1 = input("This file is not meant to be ran. Please run the main game file to play the game, or press [ENTER] to learn more")
+print("Level list")
+# Please help me come up with better names
+print("01 - Untitled")
+print("02 - Untitled")
+print("03 - Untitled")
+print("04 - Untitled")
+print("05 - Untitled")
+print("06 - Untitled")
+print("07 - Untitled")
+print("08 - Untitled")
+print("09 - Untitled")
+print("10 - Untitled")
+print("11 - Untitled")
+print("12 - Untitled")
+print("13 - Untitled")
+print("14 - Untitled")
+print("15 - Untitled")
+print("16 - Untitled")
+print("17 - Untitled")
+print("18 - Untitled")
+print("19 - Untitled")
+print("20 - Untitled")
+# I might add more levels
+noMoreExit = input("Press [ENTER] to close this file")
+print("This text should not stay on your screen. If it is, wait a bit, or close the window. If that doesn't work, kill the task with your task manager, system monitor, or terminal")
+
+'''
+File info
+
+File version: 1 (Tuesday, July 14th 2020 at 3:59 pm)
+File type: Python 3.7 (*.py)
+Line count (including blank lines and compiler line): 43
+'''
+# End of script
